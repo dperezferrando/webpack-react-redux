ccf69f614307b95e9dc5a83af72c8e2f46c249c3
dperezferrando <diego@parsimotion.com>
2020-01-25 13:06:43 -0300

    Example reducer

---
+++ client/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import testReducer from "./test";
+
+
+const rootReducer = combineReducers({
+  testReducer
+})
+
+
+export default rootReducer;
---
+++ client/reducers/test.js
@@ -0,0 +1,8 @@
+import { Types } from "../actions/example";
+
+ export default (state = { message: "TEST" }, action) => {
+    switch(action.type) {
+      case Types.TEST:
+        return {...state, message: "EXAMPLE" }
+    }
+ }
