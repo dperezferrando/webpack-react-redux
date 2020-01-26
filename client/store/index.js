b3ef71daa829d85b49bf9245a50bf8a0f7f606b6
dperezferrando <diego@parsimotion.com>
2020-01-26 15:26:41 -0300

    Reconfiguro store

--- client/store/index.js
+++ client/store/index.js
@@ -1,6 +1,14 @@
-import { createStore } from "redux";
-import rootReducer from "../reducers/index";
+import { createStore,  compose, applyMiddleware } from "redux";
+import { createEpicMiddleware } from 'redux-observable';
+import rootReducer from "../reducers";
+import rootEpic from "../epics";
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const epicMiddleware = createEpicMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(epicMiddleware));
+
+epicMiddleware.run(rootEpic);
 
 export default store;
